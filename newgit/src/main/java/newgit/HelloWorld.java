0bfd760df6789a50e6b51eae4569909f255c142c
IamMallikharjunRameshbabu <mallikarjunrameshbabu@gmail.com>
2021-12-02 09:36:40 +0530

    new

--- newgit/src/main/java/newgit/HelloWorld.java
+++ newgit/src/main/java/newgit/HelloWorld.java
@@ -1,5 +1,8 @@
-public HelloWorld{
-  public static void main(String args[]){
-   System.out.println("Hello World");
-  }
+package newgit;
+
+
+class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
 }
